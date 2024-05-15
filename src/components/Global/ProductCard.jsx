be513bff4c52c29286a323442dba61b539468528
nmukassa1 <78985121+nmukassa1@users.noreply.github.com>
2024-05-15 21:16:23 +0100

    .

--- src/components/Global/ProductCard.jsx
+++ src/components/Global/ProductCard.jsx
@@ -2,14 +2,12 @@ import {Link} from 'react-router-dom'
 
 function ProductCard({product, type}) {
     return ( 
-        // <Link to={`/book/${product.title.toLowerCase()}`} onClick={() => {localStorage.setItem('data', JSON.stringify(product))}} key={product.key} className="product-card border-solid border border-black grow shrink-0">
         <Link to={`/product?name=${product.title}`} onClick={() => {localStorage.setItem('data', JSON.stringify(product))}} key={product.key} className="product-card grow shrink-0 basis-[160px] border-solid border border-black grow shrink-0">
 
           <div id="" className="book-img w-full border-solid border border-black py-5 bg-[#F7F7F7]">
             <img src={product.img} alt="" className="w-3/4 h-full object-contain mx-auto"/>
           </div>
 
-          {/* {product.quantity > 0 && <AddToCartButton product={product} />} */} 
 
           <div id="" className={`book-info p-3 ${product.quantity === 0 ? `line-through` : ''}`}>
               <div id="book-title">
@@ -21,7 +19,6 @@ function ProductCard({product, type}) {
               <div className="book-price">
                 <span>£{product.price}</span>
               </div>
-              {/* <p>{product.quantity === 0 ? 'Out Of Stock' : 'In Stock'}</p> */}
           </div>
           
         
